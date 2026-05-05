21cbcc3876b02d2d45e5d6918d7a1402379c5c58
Emelie Jonsson <jonssonemeliemaria@gmail.com>
2026-05-05 11:34:16 +0200

    make changes to Invoice

--- Backend-project/src/main/java/se/yrgo/domains/Invoice.java
+++ Backend-project/src/main/java/se/yrgo/domains/Invoice.java
@@ -4,29 +4,31 @@ import java.time.LocalDate;
 public class Invoice {
     private String invoiceId;
     private double amount;
-    private boolean paid;
     private LocalDate invoiceDate;
 
     public Invoice(String invoiceId, double amount) {
         this.invoiceId = invoiceId;
         this.amount = amount;
-        this.paid = false;
         this.invoiceDate = LocalDate.now();
-    }
-
-    public void markAsPaid() {
-        this.paid = true;
-    }
-
-    public boolean isPaid() {
-        return paid;
     }
 
     public String getInvoiceId() {
         return invoiceId;
     }
-
     public double getAmount() {
         return amount;
+    }
+    public LocalDate getInvoiceDate() { return invoiceDate; }
+
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setInvoiceDate(LocalDate invoiceDate) {
+        this.invoiceDate = invoiceDate;
     }
 }
