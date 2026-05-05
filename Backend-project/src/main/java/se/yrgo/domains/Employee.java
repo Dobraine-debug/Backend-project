9394421759a3152894b3cf77bcf7878696d9f681
Sanna <84500411+gussannra@users.noreply.github.com>
2026-05-05 10:58:28 +0200

    Create Employee.java

---
+++ Backend-project/src/main/java/se/yrgo/domains/Employee.java
@@ -0,0 +1,52 @@
+package main.java.se.yrgo.domains;
+
+public class Employee {
+    private String employeeId;
+    private String name;
+    private String telephone;
+    private String email;
+
+    public Employee(String employeeId, String name, String telephone, String email) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee name: " + name + ", id: " + employeeId + ", telephone: " + telephone + ", email: " + email;
+    }
+}
