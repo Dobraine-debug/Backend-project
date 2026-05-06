eb47897a92250b5118bea89f691c1b5297d85c12
emeliejonsson <jonssonemeliemaria@gmail.com>
2026-05-06 14:41:45 +0200

    add relationship to customer, reservation and table

--- Backend-project/src/main/java/se/yrgo/domains/Customer.java
+++ Backend-project/src/main/java/se/yrgo/domains/Customer.java
@@ -1,30 +1,40 @@
 package se.yrgo.domains;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Customer {
-    private String customerId;
+    @Id
+    private int customerId;
     private String name;
     private String mail;
     private String telephone;
     private String invoice;
 
-    public Customer(String customerId, String name, String mail, String telephone){
+    @OneToMany(mappedBy = "customer")
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public Customer(int customerId, String name, String mail, String telephone){
         this.customerId = customerId;
         this.name = name;
         this.mail = mail;
         this.telephone = telephone;
     }
 
+    public List<Reservation> getReservations() { return reservations; }
+
     public String getCustomer(){
         return "Id: " + customerId + " Name: " + name + " Mail: " + mail + " Telephone: " + telephone;
     }
 
-    public String getCustomerId(){return customerId;}
+    public int getCustomerId(){return customerId;}
     public String getName() {return name;}
-    public String getMail(){return name;}
+    public String getMail(){return mail;}
     public String getTelephone(){return telephone;}
     public String getInvoice(){return invoice;}
 
-    public void setCustomerId(String customerId) {this.customerId = customerId;}
+    public void setCustomerId(int customerId) {this.customerId = customerId;}
     public void setName(String name) {this.name = name;}
     public void setMail(String mail) {this.mail = mail;}
     public void setTelephone(String telephone) {this.telephone = telephone;}
